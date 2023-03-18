4876db7eb4fd5be9c9c3a192ab7014ec35fa1014
lddg0502 <ldomingos@correo.unimet.edu.ve>
2023-03-18 11:47:02 -0400

    Personajes y Inteligencia Artificial

---
+++ proyecto2/src/proyecto2/Personaje.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2;
+
+/**
+ *
+ * @author Luis Domingos
+ */
+public class Personaje {
+    private String nombre;
+    private int fuerza;
+    private int inteligencia;
+    private int armas;
+    private int velocidad;
+    private int resistencia;
+    private int abilidad;
+    private boolean taken;
+    
+    public Personaje(String nombre, int fuerza, int inteligencia, int armas, int velocidad, int resistencia, int abilidad){
+        this.nombre = nombre;
+        this.fuerza = fuerza;
+        this.inteligencia = inteligencia;
+        this.abilidad = abilidad;
+        this.armas = armas;
+        this.resistencia = resistencia;
+        this.velocidad = velocidad;
+        this.taken = false;
+    }
+    
+    public void setTaken(){
+        this.taken = true;
+    }
+    public void restartTaken(){
+        this.taken = false;
+    }
+    public boolean isTaken(){
+        return this.taken;
+    }
+    public int getFuerza(){
+        return this.fuerza;
+    }
+    public int getInteligencia(){
+        return this.inteligencia;
+    }
+    public int getAbility(){
+        return this.abilidad;
+    }
+    public int getArmas(){
+        return this.armas;
+    }
+    public int getVelocidad(){
+        return this.velocidad;
+    }
+    public int getResistencia(){
+        return this.resistencia;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+}
